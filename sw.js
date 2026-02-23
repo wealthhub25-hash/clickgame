b74c2117c9f9e8b5c351cd55cdb2b74c0cc1ac71
wealthhub25-hash <wealthhub25@gmail.com>
2026-02-23 17:11:41 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,13 @@
-self.addEventListener("install", e => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open("game-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json",
-        "icon.png"
-      ]);
+    caches.open('chicken-cache').then(cache => {
+      return cache.addAll(['index.html','game.js','manifest.json','icon.png','chicken.png']);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
